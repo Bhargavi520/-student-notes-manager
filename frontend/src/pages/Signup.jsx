316dcc06bccc34384ddaf60a387d55f0bf36fa25
Dasari Bhargavi <dasarib520@gmail.com>
2026-06-04 09:50:43 +0530

    added url api to Signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -14,7 +14,7 @@ function Signup(){
     const handleSignup=async(e)=>{
         e.preventDefault();
         try{
-            await axios.post('http://localhost:5000/api/auth/signup',
+            await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signup`,
                 {name,email,password});
             navigate('/login');
             
