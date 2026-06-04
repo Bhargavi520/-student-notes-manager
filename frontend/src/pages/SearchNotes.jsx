8af2f07f63f449fce737aa9c44e06c90551e4d82
Dasari Bhargavi <dasarib520@gmail.com>
2026-06-04 09:49:54 +0530

    added url api to searchNotes

--- frontend/src/pages/SearchNotes.jsx
+++ frontend/src/pages/SearchNotes.jsx
@@ -50,7 +50,7 @@ function SearchNotes() {
             try{
 
                 const response = await axios.get(
-                    'http://localhost:5000/api/notes',{headers:{Authorization:token}}
+                    `${process.env.REACT_APP_API_URL}/api/notes`,{headers:{Authorization:token}}
                 );
 
                 setNotes(response.data);
