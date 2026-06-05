e3f0b79841fbf06ce28a3bd1436e3c5d4ec250e4
Dasari Bhargavi <dasarib520@gmail.com>
2026-06-05 06:25:41 +0530

    changed the url in searchnotes1

--- frontend/src/pages/SearchNotes.jsx
+++ frontend/src/pages/SearchNotes.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import {toast} from 'react-toastify';
-const API= import.meta.env.VITE_APP_API_URL;
+const API= import.meta.env.VITE_API_URL;
 
 function SearchNotes() {
 
