1173382a1202b48368974d69adf8907f52c5909a
Dasari Bhargavi <dasarib520@gmail.com>
2026-06-05 07:13:34 +0530

    added console.log

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 import {toast} from 'react-toastify';
 const API= import.meta.env.VITE_API_URL;
+console.log("API",API);
 
 function Signup(){
     const [name,setName]=useState('');
